9d6c161451efd3f74b71f56026274b78dfd5f14b
Anthony Medina <30554468+AnthonyMedina@users.noreply.github.com>
2019-09-10 15:53:33 +0100

    output to dist dir

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,9 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/main.js',
-  output: { file: 'index.js', format: 'esm' },
+  output: { file: 'dist/index.js', format: 'esm' },
   external: ['react'],
   plugins: [
-    babel({
-      exclude: 'node_modules/**', // only transpile our source code
-    }),
+    babel({ exclude: 'node_modules/**' }), // only transpile our source code
   ],
 };
