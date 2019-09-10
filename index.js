7d78b326a66df10c94044d087140ed3ca145f8ab
Anthony Medina <30554468+AnthonyMedina@users.noreply.github.com>
2019-09-10 12:33:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Button = function Button(_ref) {
+  var children = _ref.children;
+  return React.createElement("button", null, children);
+};
+
+var H1 = function H1(_ref) {
+  var children = _ref.children;
+  return React.createElement("h1", null, children);
+};
+
+export { Button, H1 };
---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/main.js',
+  output: { file: 'index.js', format: 'esm' },
+  external: ['react'],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
+  ],
+};
